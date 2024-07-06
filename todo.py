3e55e3298fdb5840422c7764f8684e2803f8d16e
Nitin <nitingoyal3150@gmail.com>
2024-07-07 02:12:09 +0530

    started a new project called todo list.

---
+++ todo.py
@@ -0,0 +1,21 @@
+def login():
+
+def CreateUser():
+
+def deleteUser():
+
+def printOptions():
+    print('''1: Create new user\n2: Existing User\n3: Exit''')
+
+printOptions()
+
+match int(input("Choose")):
+    case 1:
+        CreateUser()
+    case 2:
+        login()
+    case 3:
+        exit()
+    case _:
+        print("Choose correct option")
+        printOptions()
